a1a32451fc31a768a0c0761847f43d8f534a58c5
maxigonzalezf <maxigf95@gmail.com>
2025-06-19 17:03:22 -0300

    feat: fix exports and imports

--- src/components/Modal/context/ModalContext.tsx
+++ src/components/Modal/context/ModalContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, useContext, useState, type ReactNode } from "react";
+import { createContext, useState, type ReactNode } from "react";
 
-export const ModalContext = createContext<{
+const ModalContext = createContext<{
     state: boolean;
     setState: React.Dispatch<React.SetStateAction<boolean>>;
 }>({
@@ -8,18 +8,13 @@ export const ModalContext = createContext<{
     setState: () => null
 })
 
-export const ModalProvider = ({children}: {children: ReactNode}) => {
+const ModalProvider = ({children}: {children: ReactNode}) => {
     const [state, setState] = useState<boolean>(false)
 
     return <ModalContext.Provider value={{ state, setState }}>{children}</ModalContext.Provider>
 }
 
-export const useModalContext = () => {
-    const context = useContext(ModalContext);
-
-    if(!context) {
-        throw new Error("Modal is being used outside it's provider")
-    }
-
-    return context;
+export {
+    ModalContext,
+    ModalProvider
 }
